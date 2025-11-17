56951fb11421419f1189decda0fe62a053f538a0
snrahmah <snrahmah.id@gmail.com>
2025-11-17 09:07:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ for comp in components:
 # 7. Age vs Failures Scatter
 st.subheader("Age Distribution: Removed vs Not Removed")
 fig, ax = plt.subplots(figsize=(6,4))
-sns.boxplot(x='unscheduled_removal', y='hours_since_install', data=df, ax=ax, pallete="pastel")
-ax.set_xticklabel(["Not Removed", "Removed"])
+sns.boxplot(x='unscheduled_removal', y='hours_since_install', data=df, ax=ax, palete="pastel")
+ax.set_xticklabels(["Not Removed", "Removed"])
 ax.set_ylabel("Hours Since Install")
 ax.set_xlabel("")
 st.pyplot(fig)
