e9c1d7062474294f0366053f93dbed0686ea058f
snrahmah <snrahmah.id@gmail.com>
2025-11-18 13:32:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ with col1:
 
 with col2:
     # 2. Failure Count per ATA Chapter
+    df['ata_chapter'] = df['ata_chapter'].astype(str)
     failure_per_ata = df.groupby('ata_chapter')['unscheduled_removal'].sum().reset_index()
     st.subheader("Unschedulued Removal per ATA Chapter")
 
