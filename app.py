b9e5aea49234b7b7b5d1949c3591d22e1ae93acf
snrahmah <snrahmah.id@gmail.com>
2025-11-18 16:51:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ with col1:
 with col2:
     # 2. Failure Count per ATA Chapter
     df['ata_chapter'] = df['ata_chapter'].astype(str)
-    failure_per_ata = df.groupby('ata_chapter')['unscheduled_removal'].sum().reset_index()
+    failure_per_ata = df.groupby('ata_chapter')['unscheduled_removal'].sum()
     st.bar_chart(failure_per_ata)
 
 # 3. Avg Downtime per Component
